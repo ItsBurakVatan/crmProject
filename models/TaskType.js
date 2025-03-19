9ce37d6b1a51f316ada1a58c307b4a63af8e0be2
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:04:18 +0300

    Update TaskType.js

--- models/TaskType.js
+++ models/TaskType.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const taskTypeSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String, 
+        required: [true, "Görev türü adı zorunlu!"], 
+        minlength: [2, "Görev türü adı en az 2 karakter olmalı!"]
+    },
 });
 
 export default mongoose.model("TaskType", taskTypeSchema, "taskTypes");
