f2b709886886267a854451ad4f560aefce7b947f
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:02:03 +0300

    Update useFetch.js

--- frontend/src/useFetch.js
+++ frontend/src/useFetch.js
@@ -1,25 +1,22 @@
-// src/useFetch.js
-
 import { useEffect, useState } from "react";
-import axios from "axios";
+import api from "./api";
 
 const useFetch = (url) => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null); // null olarak başlatıldı
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
             try {
-
-                const res = await axios.get(url)
-
+                const res = await api.get(url);
                 setData(res.data);
             } catch (err) {
                 setError(err);
+            } finally {
+                setLoading(false);
             }
-            setLoading(false);
         };
         fetchData();
     }, [url]);
@@ -27,13 +24,13 @@ const useFetch = (url) => {
     const reFetch = async () => {
         setLoading(true);
         try {
-            const res = await axios.get(url)
-
+            const res = await api.get(url);
             setData(res.data);
         } catch (err) {
             setError(err);
+        } finally {
+            setLoading(false);
         }
-        setLoading(false);
     };
 
     return { data, loading, error, reFetch };
