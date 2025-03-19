5a7a80b7d28ff6810fedaa33a4cc54e5b599fd79
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:03:33 +0300

    Update Priority.js

--- models/Priority.js
+++ models/Priority.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const prioritySchema = new mongoose.Schema({
     name: { 
         type: String, 
-        required: [true, "Öncelik adı zorunlu"] 
+        required: [true, "Öncelik adı zorunlu!"], 
+        minlength: [2, "Öncelik adı en az 2 karakter olmalı!"]
     }
 });
 
