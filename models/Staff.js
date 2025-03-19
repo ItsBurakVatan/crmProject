18f1e0665e6595476ff682f42b52623824b87a5f
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:03:53 +0300

    Update Staff.js

--- models/Staff.js
+++ models/Staff.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 const staffSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { 
+        type: String, 
+        required: [true, "Personel adı zorunlu!"], 
+        minlength: [2, "Personel adı en az 2 karakter olmalı!"]
+    },
 });
 
-export default mongoose.model("Staff", staffSchema, "staff"); // Koleksiyon adını açıkça belirt
-
+export default mongoose.model("Staff", staffSchema, "staff");
