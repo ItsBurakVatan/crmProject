2fc314de5c6a4ca1abf569d19be7bea4c60c1165
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:03:07 +0300

    Update cities.js

--- models/cities.js
+++ models/cities.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
 
 const citySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    country: { type: mongoose.Schema.Types.ObjectId, ref: "Country", required: true },
-    plate: { type: String, required: true }, // Plaka numarası
+    name: { 
+        type: String, 
+        required: [true, "Şehir adı zorunlu!"], 
+        minlength: [2, "Şehir adı en az 2 karakter olmalı!"]
+    },
+    country: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Country", 
+        required: [true, "Ülke bilgisi zorunlu!"]
+    },
+    plate: { 
+        type: String, 
+        required: [true, "Plaka numarası zorunlu!"], 
+        minlength: [1, "Plaka numarası en az 1 karakter olmalı!"]
+    },
 });
 
 export default mongoose.model("City", citySchema, "cities");
