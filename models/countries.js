ffa99c8b63673c7c6610a32eac0978d057e48bb0
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:03:15 +0300

    Update countries.js

--- models/countries.js
+++ models/countries.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const countrySchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { 
+        type: String, 
+        required: [true, "Ülke adı zorunlu!"], 
+        minlength: [2, "Ülke adı en az 2 karakter olmalı!"]
+    },
 });
 
 export default mongoose.model("Country", countrySchema, "countries");
