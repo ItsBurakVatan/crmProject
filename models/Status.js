0fb013b4cbc2054cb5f9673c84b55c02ae406e33
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:04:00 +0300

    Update Status.js

--- models/Status.js
+++ models/Status.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const statusSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String, 
+        required: [true, "Durum adı zorunlu!"], 
+        minlength: [2, "Durum adı en az 2 karakter olmalı!"]
+    },
 });
 
 export default mongoose.model("Status", statusSchema);
