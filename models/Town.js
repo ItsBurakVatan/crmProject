e126ce26e9a5cc65e3f359d4fc889fc41f66ce8e
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:04:25 +0300

    Update Town.js

--- models/Town.js
+++ models/Town.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
 const townSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    city: { type: String, required: true }, // İl’in plaka numarası
+    name: { 
+        type: String, 
+        required: [true, "İlçe adı zorunlu!"], 
+        minlength: [2, "İlçe adı en az 2 karakter olmalı!"]
+    },
+    city: { 
+        type: String, 
+        required: [true, "Şehir bilgisi zorunlu!"]
+    },
 });
 
 export default mongoose.model("Town", townSchema, "towns");
