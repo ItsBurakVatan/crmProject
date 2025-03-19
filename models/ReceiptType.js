af55594401632a9cbc5e862ad96a866212a7edeb
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:03:46 +0300

    Update ReceiptType.js

--- models/ReceiptType.js
+++ models/ReceiptType.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const receiptTypeSchema = new mongoose.Schema({
     name: { 
         type: String, 
-        required: [true, "Fiş türü adı zorunlu"] 
+        required: [true, "Fiş türü adı zorunlu!"], 
+        minlength: [2, "Fiş türü adı en az 2 karakter olmalı!"]
     }
 });
 
