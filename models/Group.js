1b30322b11ea05f9881a3a8884e1406ead512957
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:03:24 +0300

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const groupSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String, 
+        required: [true, "Grup adı zorunlu!"], 
+        minlength: [2, "Grup adı en az 2 karakter olmalı!"]
+    },
 });
 
 export default mongoose.model("Group", groupSchema);
