e09daff29dbd4d6f2dc83d803b430223c5fc8709
Burak Vatan <82179253+ItsBurakVatan@users.noreply.github.com>
2025-03-19 12:05:48 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { login, register } from "../controllers/user.js";
+import { register, login, getUsers, updateUserRole } from "../controllers/user.js";
+import { authorize } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/", authorize("admin"), getUsers); // Kullanıcıları listele
+router.put("/:id/role", authorize("admin"), updateUserRole); // Rol güncelle
 
 export default router;
